f6b9fff94eacd4a39fdd8482ba841865782f3021
yusufdestan <yusuf.destan@vub.be>
2021-08-02 19:00:49 +0200

    added long_description_content_type

--- setup.py
+++ setup.py
@@ -7,6 +7,11 @@ def readme():
     with open('README.md', 'r') as f:
         return f.read()
 
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+    
 # package configuration - for reference see:
 # https://setuptools.readthedocs.io/en/latest/setuptools.html#id9
 setuptools.setup(
@@ -14,6 +19,7 @@ setuptools.setup(
     version=py_peak_splitting.__version__,    
     description='Utilities to de-noise time series from random telegraph noise / peak splitting artefacts',
     long_description=readme(),
+    long_description_content_type='text/markdown',
     url='https://github.com/OWI-Lab/py_peak_splitting',
     author='D.J.M. Fallais',
     author_email='dominik.fallais@vub.be',
