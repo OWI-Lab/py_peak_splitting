7ae520dd1b68181056c8cab68b1e4809cf5849ee
yusufdestan <yusuf.destan@vub.be>
2021-08-02 19:08:34 +0200

    update the classifiers at setup.py for pypi

--- setup.py
+++ setup.py
@@ -11,14 +11,14 @@ from os import path
 this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
-    
+
 # package configuration - for reference see:
 # https://setuptools.readthedocs.io/en/latest/setuptools.html#id9
 setuptools.setup(
     name='py_peak_splitting',
     version=py_peak_splitting.__version__,    
     description='Utilities to de-noise time series from random telegraph noise / peak splitting artefacts',
-    long_description=readme(),
+    long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/OWI-Lab/py_peak_splitting',
     author='D.J.M. Fallais',
@@ -28,9 +28,9 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     install_requires=['numpy', 'matplotlib','plotly','sklearn',],
     classifiers=[
-        'Development Status :: 1 - Planning',
-        'Intended Audience :: Research/Applied Sciences/Indsutry',
-        'License :: OSI Approved :: CC-NC-BY-SA 4.0',  
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        #'License :: OSI Approved :: CC-NC-BY-SA 4.0',  
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
